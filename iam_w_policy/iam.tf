2eefb394ffedfb42f08823878d6d2f10276bb3b1
kliliia <62531900+kliliia@users.noreply.github.com>
2020-03-24 19:42:50 -0500

    Add files via upload

---
+++ iam_w_policy/iam.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_user" "user" {
+  name = "test-user"
+}
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+ policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ec2:Describe*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = "${aws_iam_user.user.name}"
+  policy_arn = "${aws_iam_policy.policy.arn}"
+}
